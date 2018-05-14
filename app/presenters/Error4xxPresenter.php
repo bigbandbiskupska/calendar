517d57e8dadf0d67c25e1f5c609c76428f2376b9
Kryštof Tulinger <k.tulinger@seznam.cz>
2018-05-14 08:40:39 +0200

    adding 404 error log

--- app/presenters/Error4xxPresenter.php
+++ app/presenters/Error4xxPresenter.php
@@ -21,6 +21,8 @@ class Error4xxPresenter extends BasePresenter
 		// load template 403.latte or 404.latte or ... 4xx.latte
 		$file = __DIR__ . "/templates/Error/{$exception->getCode()}.latte";
 		$file = is_file($file) ? $file : __DIR__ . '/templates/Error/4xx.latte';
+		Debugger::log(
+                	sprintf("%s '%s%s' -> 404 from '%s' (%s) at '%s'", $_SERVER['REQUEST_METHOD'], $_SERVER['HTTP_HOST'], $_SERVER['REQUEST_URI'], isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : 'uknown referer', isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : 'uknown agent', date('Y-n-d H:i:s', $_SERVER['REQUEST_TIME'])), Debugger::ERROR);
 		$this->template->setFile($file);
 	}
 }
